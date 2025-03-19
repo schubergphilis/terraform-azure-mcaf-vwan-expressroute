1f41975507b6c8e6de8dea82589c54004254657c
Gillian Stravers <gillianstravers@icloud.com>
2025-03-19 14:43:51 +0100

    add maps

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "tags" {
 
 variable "express_route_circuit" {
   description = "The Express Route Circuit to create"
-  type = object({
+  type = map(object({
     name                   = optional(string)
     bandwidth_in_mbps      = optional(number)
     peering_location       = optional(string)
@@ -24,41 +24,41 @@ variable "express_route_circuit" {
     sku_tier               = optional(string)
     sku_family             = optional(string)
     authorization_key_name = optional(string)
-  })
+  }))
   default = {}
 }
 
 variable "express_route_circuit_peering" {
   description = "The Express Route Circuit Peering to create. Circuit name is used if you don't deploy the Express Route Circuit from this module"
 
-  type = object({
+  type = map(object({
     primary_peer_address_prefix   = optional(string)
     secondary_peer_address_prefix = optional(string)
     vlan_id                       = optional(number)
     shared_key                    = optional(string)
     peer_asn                      = optional(number)
-  })
+  }))
   default = {}
 }
 
 variable "express_route_gateway" {
   description = "The Express Route Gateway to create"
-  type = object({
+  type = map(object({
     name                          = optional(string)
     scale_units                   = optional(number)
     virtual_hub_id                = optional(string)
     allow_non_virtual_wan_traffic = optional(bool)
-  })
+  }))
   default = {}
 }
 
 variable "express_route_gateway_connection" {
   description = "The Express Route Gateway Connection to create"
-  type = object({
+  type = map(object({
     name               = optional(string)
     er_gateway_id      = optional(string)
     circuit_peering_id = optional(string)
     authorization_key  = optional(string)
-  })
+  }))
   default = {}
 }
